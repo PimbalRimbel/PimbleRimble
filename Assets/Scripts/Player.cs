fe81513cb0d2d25f97a0db4ac085770bb4adebed
lauravazquezpereda <micurso3y4@gmail.com>
2024-05-18 18:28:08 +0200

    cositas

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D Rigidbody2D;
+
+    private int numero = 0;
+
+
+    //PATRÓN OBSERVER
+    private List<IObserver<float>> observers = new List<IObserver<float>>();
+
+    public void AddObserver (IObserver<float> observer) //agregamos
     {
-        
+        observers.Add(observer);
+    }
+
+    public void RemoveObserver (IObserver<float> observer) //eliminamos
+    {
+        observers.Remove(observer);
+    }
+
+    public void NotifyObserver()
+    {
+        foreach(IObserver<float> observer in observers)
+        {
+            observer?.UpdateObserver(numero);
+        }
+    }
+
+
+    public void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
